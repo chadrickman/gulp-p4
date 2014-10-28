f6ef083d8e39778f50a463a17bca9cd49efbd4aa
wokim <wonshikkim.kr@gmail.com>
2014-10-28 14:55:28 +0900

    New implements used in node-perforce

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
 'use strict';
 
+var p4 = require('node-perforce');
 var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
-var exec = require('child_process').exec;
 
 var PLUGIN_NAME = 'gulp-p4';
 
-function optionMapper(options) {
-  if (!options) return '';
-  var optionMaps = {
-    changelist: {cmd: '-c', args: true},
-    filetype: {cmd: '-t', args: true},
-    force: {cmd: '-f', args: false},
-    description: {cmd: '-i', args: true}
-  };
-  return Object.keys(options).map(function (option) {
-    var map = optionMaps[option];
-    if (!map || !options[option]) return;
-    return map.cmd + (map.args ? ' ' + options[option] : '');
-  }).join(' ');
-}
-
-function execP4(p4cmd, options, files, callback) {
-  var cmd = [];
-  cmd.push('p4');
-  cmd.push(p4cmd);
-  cmd.push(optionMapper(options));
-  cmd.push(files);
-  console.log(cmd.join(' '));
-  exec(cmd.join(' '), callback);
-}
-
 module.exports = function (p4cmd, options) {
-  if (!p4cmd) {
-    throw new PluginError(PLUGIN_NAME, 'Missing p4 command');
-  }
-  var stream = through.obj(function (file, enc, cb) {
-    execP4(p4cmd, options, file.history, function (err, stdout, stderr) {
+  if (!p4cmd) throw new PluginError(PLUGIN_NAME, 'Missing p4 command');
+  if (!p4[p4cmd]) throw new PluginError(PLUGIN_NAME, 'Invalid command');
+
+  return through.obj(function (file, enc, callback) {
+    options = options || {};
+    options.files = [file.history];
+    p4[p4cmd](options, function(err) {
       if (err) throw new PluginError(PLUGIN_NAME, err);
-      if (stderr) throw new PluginError(PLUGIN_NAME, stderr);
-      cb();
+      callback();
     });
   });
-  return stream;
 };
